99110d730ef9e7115c8fd6f4f464615c9d9bbcf8
Jimena Hernández <her21199@uvg.edu.gt>
2025-11-14 08:21:18 -0600

    fix[ui]: wha component

---
+++ src/components/WhatsApp.jsx
@@ -0,0 +1,24 @@
+"use client"
+
+const WhatsApp = () => {
+  return (
+    <section className="" id="wp">
+    <a
+        href="https://wa.me/00000000"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-5 right-5 z-50 animate-float"
+      >
+        <div className="bg-white p-3 md:p-4 rounded-full shadow-lg hover:bg-green-400 transition-all duration-300">
+          <img
+            src="./images/whatsapp.png"
+            alt="WhatsApp"
+            className="w-10 md:w-14"
+          />
+        </div>
+      </a>
+    </section>
+  )
+}
+
+export default WhatsApp
