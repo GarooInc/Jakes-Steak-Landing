5c6464bd4e2655693346e80313aacf3d444444c5
faguilarleal <102198566+faguilarleal@users.noreply.github.com>
2025-11-18 12:34:19 -0600

    feat[contactUs]: add links

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -18,27 +18,47 @@ const ContactUs = () => {
       <div className="">
         {/* Right Side - Contact Options */}
         <div className="bg-[#f2b92b] flex  flex-row  items-center justify-center gap-5 px-3 md:px-8 py-10 md:py-16 league-gothic relative">
-          
+
+          <a
+              href="https://share.google/yAZEC1lOGxoEN6fVt"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-1 md:gap-4 text-[#1c5234] cursor-pointer"
+            >
           <div className="flex items-center gap-1 md:gap-4 text-[#1c5234] ">
             <div className="w-10 md:w-[60px] flex justify-center">
               <img src="./images/visit-us.png" className=" w-10 md:w-[77px]" alt="" />
             </div>
             <span className="text-base md:text-[38px] uppercase text-nowrap">Come Visit Us</span>
-          </div>
+          </div></a>
 
+
+          <a
+              href="mailto:info@jakesnysteak.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-1 md:gap-4 text-[#1c5234] cursor-pointer"
+            >
           <div className="flex items-center gap-1 md:gap-4 text-[#1c5234] ">
             <div className="w-[30px] md:w-20 flex justify-center">
               <img src="./images/message.png" className=" w-[60px]" alt="" />
             </div>
             <span className="text-base md:text-[38px] uppercase text-nowrap">Write To Us</span>
-          </div>
+          </div></a>
 
-          <div className="flex items-center gap-1 md:gap-4 text-[#1c5234] ">
-            <div className="w-[30px] md:w-[80px] flex justify-center">
-              <img src="./images/instagram.png" className="md:w-[50px] " alt="" />
-            </div>
-            <span className="text-base md:text-[38px] uppercase text-nowrap">Take a Look</span>
-          </div>
+           <a
+              href="https://www.instagram.com/jakes.new.york.steak/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-1 md:gap-4 text-[#1c5234] cursor-pointer"
+            >
+              <div className="w-[30px] md:w-[80px] flex justify-center">
+                <img src="./images/instagram.png" className="md:w-[50px]" alt="" />
+              </div>
+              <span className="text-base md:text-[38px] uppercase text-nowrap">
+                Take a Look
+              </span>
+            </a>
               {/* <img src="./images/whatsapp.png" className="hidden md:block w-[77px] bottom-14 right-10 absolute" alt="" /> */}
         </div>
 
